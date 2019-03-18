591300ac8275301d5578d618aa8bf2d960b2272f
Srijan Garg <srijangarg24398@gmail.com>
2019-03-18 07:41:07 +0530

    Weather app

---
+++ assets/config.ts
@@ -0,0 +1,7 @@
+export default {
+    'weathermap_api': 'https://api.openweathermap.org/data/2.5/',
+    'forecast_api': 'https://api.forecast.io/forecast/',
+    'dark_sky_api': 'https://api.darksky.net/forecast/',
+    'weathermap_key': '188b68e6b443a5380ce7ee0f0bb49cfc',
+    'darksky_key': '77de2b5288f69e61fb2f922a7dc0b075'
+};
